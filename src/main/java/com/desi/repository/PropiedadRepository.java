c45014525de4b03cf3e8346484badb80e349592c
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2026-06-09 20:16:31 -0300

    avances con html carpetas repository service

---
+++ src/main/java/com/desi/repository/PropiedadRepository.java
@@ -0,0 +1,8 @@
+package com.desi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.desi.entity.Propiedad;
+
+public interface PropiedadRepository extends JpaRepository<Propiedad, Long> {
+
+}
